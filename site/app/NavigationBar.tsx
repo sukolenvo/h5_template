76dd0fce9fd1d608a00ea5ffaca1b0265da705b4
Vadym <sukolenvo@gmail.com>
2024-02-02 21:27:22 +0800

    fix toolbar color

--- site/app/NavigationBar.tsx
+++ site/app/NavigationBar.tsx
@@ -5,7 +5,7 @@ import "./NavigationBar.css"
 export function NavigationBar() {
   const pathname = usePathname()
   return (
-    <div className="fixed top-0 left-0 right-0 h-20 items-center flex text-l font-semibold navigationBar shadow-md bg-white z-10 justify-center">
+    <div className="fixed top-0 left-0 right-0 h-20 items-center flex text-l font-semibold navigationBar shadow-md z-10 justify-center">
       <Link href="/lethos/" className={`link hover:opacity-75 ${pathname === '/lethos' ? '' : 'opacity-50'}`}>Lethos</Link>
       <Link href="/crown/" className={`link hover:opacity-75 ${pathname === '/crown' ? '' : 'opacity-50'}`}>Crown</Link>
       <Link href="/mask/" className={`link hover:opacity-75 ${pathname === '/mask' ? '' : 'opacity-50'}`}>Mask</Link>
