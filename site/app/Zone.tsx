c1e0137d6ff1dfda37529b5adea4c290a372da9e
Vadym <sukolenvo@gmail.com>
2024-03-09 10:58:40 +0800

    add stables, star axis, black market; minor image style fix

--- site/app/Zone.tsx
+++ site/app/Zone.tsx
@@ -13,7 +13,7 @@ export type ObjectProp = {
 export function ObjectComponent({object}: ObjectProp) {
   return (
     <div className="flex flex-row items-center">
-      <Image className="m-1" style={{borderRadius: '25%'}} src={`/img/${object.name}.png`} alt={object.name} width={64} height={64}/>
+      <Image className="m-1" style={{borderRadius: '25%', maxHeight: '64px', objectFit: "contain"}} src={`/img/${object.name}.png`} alt={object.name} width={64} height={64}/>
       <div >
         <p>{object.name}: {Math.round(object.chance * 100) + "% "}
           {object.maxNumber > 1 ? " x" + object.maxNumber : ""}
@@ -28,11 +28,11 @@ export function ObjectComponent({object}: ObjectProp) {
   )
 }
 
-const excludeItems = ["Rally_Flag", "RefugeeCamp", "Water_Wheel", "Stables",
+const excludeItems = ["Rally_Flag", "RefugeeCamp", "Water_Wheel",
   "Prison", "SacrificeAltar", "Oasis", "Garden_of_the_Wee_Folk",
-  "Fountain_Of_Youth", "Temple", "Windmill", "Den_Of_Thieves", "Magic_Well", "Fountain_Of_Fortune", "Star_Axis", "Idol_Of_Fortune",
+  "Fountain_Of_Youth", "Temple", "Windmill", "Den_Of_Thieves", "Magic_Well", "Fountain_Of_Fortune", "Idol_Of_Fortune",
 "House_Of_Astrologer", "Mummy_dwell_new", "Sanctuary", "Dark_knight_new",
-"Eye_Of_Magi3", "Black_Market", "Fortuitous_Sanctuary", "Wolf_dwell_new", "Manticore_Cave_new", "Eye_Of_Magi1", "Eye_Of_Magi2",
+"Eye_Of_Magi3", "Fortuitous_Sanctuary", "Wolf_dwell_new", "Manticore_Cave_new", "Eye_Of_Magi1", "Eye_Of_Magi2",
   "Hut_Of_Magi1", "Hut_Of_Magi2", "Magic_Spring", "Hut_Of_Magi3", "Faerie_Ring", "Tavern",
   "Sawmill", "Ore_Pit", "Gold_Mine", "Sulfur_Dune", "Crystal_Cavern", "Gem_Pond", "Alchemist_Lab", "Abandoned_Mine"]
 
