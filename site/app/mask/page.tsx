eddd20aac53fd4943baf6b90ef6c99ee68155625
Vadym <sukolenvo@gmail.com>
2024-10-06 16:37:43 +0800

    eye narrative for mask; fixes to mast template

--- site/app/mask/page.tsx
+++ site/app/mask/page.tsx
@@ -5,76 +5,79 @@ import {NavigationBar} from "@/app/NavigationBar";
 import {useWindowSize} from "@/app/util";
 import zones from "./mask.json";
 
+const colorZoneStart = "#FF0000"
+const colorZonePromGrass = "#ff8f00"
+const colorZonePromNotGrass = "#bfbf00"
+const colorZoneSmej = "#80FF00"
+const colorZoneTown = "#00ffFF"
+const colorZoneGold = "#0040ff"
+const colorZoneMT = "#8000ff"
+const colorZoneTreasure = "#FF00bF"
+
 const templateConfig: TemplateConfig = {
   zoneGroups: [
     {
       name: "Стартовые",
-      color: "#FF0000",
+      color: colorZoneStart,
       zoneNumbers: [1, 2],
       objectGroup: 0
     },
     {
-      name: "Промежуточные (3,5)",
-      color: "#ff8f00",
+      name: "Промзона (3,5)",
+      color: colorZonePromGrass,
       zoneNumbers: [3, 5],
       objectGroup: 0
     },
     {
-      name: "Промежуточные (4,6)",
-      color: "#bfbf00",
+      name: "Промзона (4,6)",
+      color: colorZonePromNotGrass,
       zoneNumbers: [4, 6],
       objectGroup: 0
     },
     {
-      name: "Смежные Вторички (7,9)",
-      color: "#80ff00",
+      name: "Смежка (7,9)",
+      color: colorZoneSmej,
       zoneNumbers: [7, 9],
       objectGroup: 0
     },
     {
-      name: "Вторичка (8)", // center top
-      color: "#00FF40",
-      zoneNumbers: [8],
-      objectGroup: 0
-    },
-    {
-      name: "Вторичка (10)", // center bottom
-      color: "#00ffFF",
-      zoneNumbers: [10],
+      name: "Вторик", // center top
+      color: colorZoneTown,
+      zoneNumbers: [8, 10],
       objectGroup: 0
     },
     {
       name: "Золотая",
-      color: "#0040ff",
+      color: colorZoneGold,
       zoneNumbers: [11],
       objectGroup: 0
     },
     {
-      name: "Минитрежари",
-      color: "#8000ff",
+      name: "Минитрежери",
+      color: colorZoneMT,
       zoneNumbers: [12],
       objectGroup: 0
     },
     {
       name: "Трежери",
-      color: "#FF00bF",
+      color: colorZoneTreasure,
       zoneNumbers: [13, 14],
       objectGroup: 0
     }
   ],
   templateImage: "/mask.drawio.svg",
   description: [
-    "1,2 - стартовые зоны, руда +дерево, двеллы, повышалки",
-    "3,5 - промежуточная трава",
-    "4,6 - промежуточная зона, случайная земля",
-    "7,8,9 - зоны с вторичками, чуть артов",
-    "11 - зона с двумя золотыми шахтами, средние арты, столбы и рощи и тд",
+    "Все неподписанные - охрана 70",
+    "1, 2 - стартовые зоны, руда +дерево, двеллы, повышалки",
+    "3, 5 - промзона всегда трава трава",
+    "4, 6 - промзона зона - случайная земля (может трава, а может и нет)",
+    "7, 9 - смежка",
+    "8, 10 - вторик",
+    "11 - золотая - золотая шахта, средние арты, столбы и рощи и тд",
     "12 - минитрежа (средние арты, есть приличные сокровищницы, нет утоп)",
-    "13,14 - трежери, хорошие арты, утопы",
+    "13, 14 - трежери, хорошие арты, утопы",
   ],
-  // 1: 3 4 5 6 -> 7 8 9 10 11
-  // 2: 7 8 9 10 11 12 -> 12
-  // 3: -> 1 2 13 14
+  eyeNarrative: EyeNarrative(),
 }
 
 export default function Page() {
@@ -88,5 +91,34 @@ export default function Page() {
         }
       </div>
     </main>
+  )
+}
+
+// 1: 3 4 5 6 -> 7 8 9 10
+// 2: 7 8 9 10 11 12 -> 12 11
+// 3: -> 1 2 13 14
+function EyeNarrative() {
+  return (
+    <div className="flex flex-row space-x-1 relative eyeNarrative">
+      <img src={"/img/Eye_Of_Magi.png"} alt="Eye Of Magi"
+           style={{maxWidth: "24px", objectFit: "contain", objectPosition: "top"}}/>
+      <div>
+        <p>Око мага во всех промзонах (<span className={"zoneName"}
+                                             style={{borderColor: colorZonePromGrass}}>3, 5</span>,&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZonePromNotGrass}}>4, 6</span>)
+          показывает&nbsp;<span className={"zoneName"} style={{borderColor: colorZoneSmej}}>Смежки</span>&nbsp;(7, 9)
+          и&nbsp;<span className={"zoneName"}
+                       style={{borderColor: colorZoneTown}}>Вторики</span>&nbsp;(8, 10)
+        </p>
+        <p>
+          <span className={"zoneName"} style={{borderColor: colorZoneSmej}}>Смежки</span>&nbsp;(7, 9),&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZoneTown}}>Вторики</span>&nbsp;(8, 10),&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZoneGold}}>Золотая</span>&nbsp;(11) и&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZoneMT}}>Минитрежа</span>&nbsp;(12) показывают&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZoneGold}}>Золотую</span>&nbsp;(11) и&nbsp;<span
+          className={"zoneName"} style={{borderColor: colorZoneMT}}>Минитрежери</span>&nbsp;(12)
+        </p>
+      </div>
+    </div>
   )
 }
